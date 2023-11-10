a18862a3fdf17326f4a64eb59be3e5406bbb61f2
marta_rutkowska <marta.rutkowska.cs@gmail.com>
2023-11-10 17:31:57 +0100

    user input validation

--- main/main.go
+++ main/main.go
@@ -1,26 +1,30 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"net/url"
 )
 
 func main() {
 
 	var userUrl string
 	fmt.Println("Hello there, provide the url u wish to shorten.")
-	fmt.Scanf("%s", &userUrl)
 
-	if urlValid(userUrl) {
-		fmt.Println("your url is", userUrl)
+	for {
+		_, err := fmt.Scanf("%s \n", &userUrl)
+		if err == nil && urlValid(userUrl) {
+			break
+		} else if err != nil {
+			fmt.Println("U need to provide one argument")
+		} else if err == nil && !urlValid(userUrl) {
+			fmt.Println("your url is invalid! provide a valid one")
+		}
 	}
 
+	return
 }
 
-func urlValid(user_url string) bool {
-	if len(user_url) == 0 {
-		errors.New("provide non empty url")
-		return false
-	}
-	return true
+func urlValid(userUrl string) bool {
+	_, err := url.ParseRequestURI(userUrl)
+	return err == nil
 }
