18e8d86ede3067df2d0c0f06cd3d4f4c6c4e76e9
marta_rutkowska <marta.rutkowska.cs@gmail.com>
2023-11-11 13:45:23 +0100

    user console options

--- main/main.go
+++ main/main.go
@@ -3,12 +3,51 @@ package main
 import (
 	"fmt"
 	"net/url"
+	"slices"
 )
 
 func main() {
 
+	var userSelectedOption = displayOptions()
+
+	if userSelectedOption != "" {
+		fmt.Printf("userUrl: %s", userSelectedOption)
+	}
+
+	return
+}
+
+func displayOptions() string {
+
 	var userUrl string
-	fmt.Println("Hello there, provide the url u wish to shorten.")
+	var userOption int
+	options := []int{1, 2, 3}
+
+	fmt.Println("Hello, please choose one of the options:")
+	fmt.Println("1 - shorten the url")
+	fmt.Println("2 - return the full url from shortened form")
+	fmt.Println("3 - escape program")
+
+	_, err := fmt.Scanf("%d \n", &userOption)
+	if err != nil || !slices.Contains(options, userOption) {
+		fmt.Println("No valid option chosen, escaping")
+	}
+
+	switch userOption {
+	case 1:
+		userUrl = readUserUrl()
+	case 2:
+		userUrl = readUserUrl()
+	default:
+	}
+
+	return userUrl
+}
+
+func readUserUrl() string {
+
+	var userUrl string
+	fmt.Println("Provide the url u wish to shorten/get full version of.")
 
 	for {
 		_, err := fmt.Scanf("%s \n", &userUrl)
@@ -21,7 +60,7 @@ func main() {
 		}
 	}
 
-	return
+	return userUrl
 }
 
 func urlValid(userUrl string) bool {
